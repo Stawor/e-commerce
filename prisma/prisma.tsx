cc4607f97d9bb4961ee41e4fbce6335d17464be4
Stawor <staworkogabriel@gmail.com>
2023-11-30 03:59:14 +0100

    schema changed

--- prisma/prisma.tsx
+++
@@ -1,9 +0,0 @@
-import { PrismaClient, Prisma } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-export default async function Prisma() {
-	const items = await prisma.item.findMany();
-
-	return <>{items.map((item) => item.name)}</>;
-}
